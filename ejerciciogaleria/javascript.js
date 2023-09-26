2e5b06adc409ff0afe4587f2b56c2bc4b2e8dd15
d4rkpsych0p4th <33335436+d4rkpsych0p4th@users.noreply.github.com>
2023-09-26 11:54:21 +0200

    Update javascript.js

--- ejerciciogaleria/javascript.js
+++ ejerciciogaleria/javascript.js
@@ -1,10 +1,6 @@
-const images = [
-    'imagen1.jpg',
-    'imagen2.jpg',
-    'imagen3.jpg'
-];
-
+const images = ['imagen1.jpg','imagen2.jpg','imagen3.jpg'];
 let currentIndex = 0;
+
 const imageContainer = document.querySelector('.image-container');
 const prevButton = document.getElementById('prevBtn');
 const nextButton = document.getElementById('nextBtn');
@@ -18,11 +14,11 @@ prevButton.addEventListener('click', () => {
     currentIndex = (currentIndex - 1 + images.length) % images.length;
     updateImage();
 });
-
+//document.getElementById("prevButton").onclick =prev;
 nextButton.addEventListener('click', () => {
     currentIndex = (currentIndex + 1) % images.length;
     updateImage();
 });
-
+//document.getElementById("nextButton").onclick =next;
 // Mostrar la primera imagen al cargar la página
 updateImage();
