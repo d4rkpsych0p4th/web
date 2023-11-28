ec7c9ae14b0289a95bb6f751c0befac6af5ce193
d4rkpsych0p4th <33335436+d4rkpsych0p4th@users.noreply.github.com>
2023-11-28 20:32:34 +0100

    Update page.jsx

--- next/next2/app/user/page.jsx
+++ next/next2/app/user/page.jsx
@@ -1,13 +1,13 @@
 import Card from "@/components/Card";
-async function getProps(){
+async function loaddata(){
     const res = await fetch("https://reqres.in/api/users");
-    const props = await res.json();
-    return props.data;
+    const dats = await res.json();
+    return dats.data;
 }
 
 
 export default async function Page() {
-    const user = await getProps();
+    const user = await loaddata();
 return <div>{
     
     user.map(user=>(
