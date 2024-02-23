9f6da0561fdb542a04154427859d28c3124148b5
d4rkpsych0p4th <33335436+d4rkpsych0p4th@users.noreply.github.com>
2024-02-23 18:17:58 +0100

    update
    -implements upload jpg
    -fix tracks no funcionaba

--- Q2/MVC/app.js
+++ Q2/MVC/app.js
@@ -5,7 +5,8 @@ require('dotenv').config();
 const app = express()//Le decimos a la app de express() que use cors para evitar el error Cross-Domain (XD)
 app.use(cors())
 app.use(express.json())
-app.use("/api", require("./routes")) //Lee routes/index.js por defecto
+app.use("/api", require("./routes"))
+app.use(express.static("storage"))//localhost:3000/file.jpg
 const port = process.env.PORT || 3000
 dbConnect()
 app.listen(port, () => {
