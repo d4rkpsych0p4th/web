34b7987f4dee3ebfe386cccb7983f9bc4302d56b
d4rkpsych0p4th <33335436+d4rkpsych0p4th@users.noreply.github.com>
2024-02-27 09:30:00 +0100

    update

--- Q2/MVC/routes/tracks.js
+++ Q2/MVC/routes/tracks.js
@@ -1,9 +1,14 @@
 
 const express = require("express")
 const router = express.Router()
+//controller load
 const { getItems, getItem, createItem} = require("../controllers/tracks")
-
+//validator load 
+const { validatorCreateItem } = require("../validators/tracks")
+//controller links
 router.get("/", getItems)
 router.get("/:id", getItem)
-router.post("/", createItem)
+//validator link
+router.post("/", validatorCreateItem, createItem)
+
 module.exports = router
