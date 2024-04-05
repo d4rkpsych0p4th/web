73db492947bb48b0d07f9559fe1387d57ca69a20
d4rkpsych0p4th <33335436+d4rkpsych0p4th@users.noreply.github.com>
2024-04-05 12:15:39 +0200

    swagger

--- Q2/MVC/app.js
+++ Q2/MVC/app.js
@@ -2,12 +2,15 @@ require('dotenv').config();
 const express = require("express")
 const dbConnect = require('./config/mongo')
 const cors = require("cors")
+const swaggerUi = require("swagger-ui-express")
+const swaggerSpecs = require("./docs/swagger")
 const { sequelize, dbConnectMySql } = require("./config/mysql")
 const morganBody = require("morgan-body")
 const {IncomingWebhook} = require("@slack/webhook")
 const app = express()//Le decimos a la app de express() que use cors para evitar el error Cross-Domain (XD)
 app.use(cors())
 app.use(express.json())
+app.use("/api-docs",swaggerUi.serve,swaggerUi.setup(swaggerSpecs))
 app.use("/api", require("./routes"))
 app.use(express.static("storage"))//localhost:3000/file.jpg
 
@@ -41,3 +44,5 @@ morganBody(app, {
     },
     stream: loggerStream
 })
+
+module.exports = app
--- Q2/MVC/routes/auth.js
+++ Q2/MVC/routes/auth.js
@@ -4,14 +4,85 @@ const {validatorRegister, validatorLogin} = require("../validators/auth")
 const router = express.Router()
 const authMiddleware = require("../middleware/session")
 const checkRol = require("../middleware/rol")
+const { getItems } = require("../controllers/users")
+
+/*** @openapi
+* /api/auth/register:
+*  post:
+*      tags:
+*      - User
+*      summary: "User registter"
+*      description: Register a new user
+*      requestBody:
+*          content:
+*              application/json:
+*                  schema:
+*                      $ref: "#/components/schemas/user"
+*      responses:
+*          '200':
+*              description: Returns the inserted object
+*          '401':
+*              description: Validation error
+*      security:
+*          - bearerAuth: []
+*/
 
 //POST http://localhost:3000/api/auth/register
 router.post("/register", validatorRegister, registerCtrl)
 
+/*** @openapi
+* /api/auth/login:
+*  post:
+*      tags:
+*      - User
+*      summary: "Login user"
+*      description: login user
+*      requestBody:
+*          content:
+*              application/json:
+*                  schema:
+*                      $ref: "#/components/schemas/login"
+*      responses:
+*          '200':
+*              description: Returns the inserted object
+*          '401':
+*              description: Validation error
+*/
 //POST http://localhost:3000/api/auth/login
+
 router.post("/login", validatorLogin, loginCtrl) 
+
+
+/*** @openapi
+* /api/auth/update/{id}:
+*  post:
+*      tags:
+*      - User
+*      summary: Update user
+*      description: Update a user by an admin
+*      parameters:
+*          -   name : id
+*              in : path
+*              description: id that need to be updated
+*              required : true
+*              schema:
+*                  type: string
+*      requestBody:
+*          content:
+*              application/json:
+*                  schema:
+*                      $ref: "#/components/schemas/user"
+*      responses:
+*          '200':
+*              description: Returns the inserted object
+*          '401':
+*              description: Validation error
+*      security:
+*          - bearerAuth: []
+*/
 //PUT  http://localhost:3000/api/auth/:id
 router.put("/update/:email",authMiddleware,checkRol(["admin"]), updateUser) 
 //router.put("/:id",authMiddleware,checkRol(["admin"]), updateUser) 
+
 
 module.exports = router
